06389f4df6f002f997f2a6ed890c315fd0ed1f25
Frederik Elwert <frederik.elwert@rub.de>
2024-09-13 12:46:25 +0200

    Add images to item metadata

--- pelican/plugins/collection_builder/collection_builder.py
+++ pelican/plugins/collection_builder/collection_builder.py
@@ -1,10 +1,40 @@
-import datetime
 import csv
+import datetime
+import mimetypes
 from pathlib import Path
 
 from pelican import signals
 from pelican.contents import Article
 from pelican.readers import BaseReader
+
+
+def _is_image(path):
+    mtype, enc = mimetypes.guess_type(path)
+    if mtype is None:
+        return False
+    if mtype.split("/")[0] == "image":
+        return True
+    return False
+
+
+def add_image(row, settings):
+    content_path = Path(settings["PATH"])
+    raw_images_path = content_path / "images"
+    pid = row["pid"]
+    # Test single image
+    matches = list(raw_images_path.glob(f"{pid}.*"))
+    if len(matches) == 1 and _is_image(matches[0]):
+        image = matches[0]
+        return {"image": f"/{image.relative_to(content_path)}"}
+    # Test folder of images
+    item_path = raw_images_path / pid
+    if item_path.is_dir():
+        images = [f for f in item_path.iterdir() if _is_image(f)]
+        return {
+            "images": [
+                f"/{image.relative_to(content_path)}" for image in images
+            ]
+        }
 
 
 def add_csv_items(generator):
@@ -15,6 +45,7 @@ def add_csv_items(generator):
     with csv_file_path.open(newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            # Map basic metadata
             metadata = {
                 "title": row["label"],
                 "date": datetime.datetime.now(),
@@ -22,7 +53,11 @@ def add_csv_items(generator):
                 "category": base_reader.process_metadata("category", "Collection"),
                 **row,
             }
+            # Add image paths
+            metadata.update(add_image(row, generator.settings))
+            # Add content
             content = ""
+            # Generate and insert article
             article = Article(content, metadata, settings=generator.settings)
             generator.articles.append(article)
 
